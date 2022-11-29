4cdc20551eb4c57562dec05da9c6c6b16d8fe0e6
rrodrigodlima <rdrigo019@gmail.com>
2022-11-29 04:41:09 -0300

    requisito 8

--- src/main.js
+++ src/main.js
@@ -1,23 +1,44 @@
+import { saveCartID } from './helpers/cartFunctions';
 import { searchCep } from './helpers/cepFunctions';
-import { fetchProductsList } from './helpers/fetchFunctions';
-import { createProductElement } from './helpers/shopFunctions';
+import { fetchProduct, fetchProductsList } from './helpers/fetchFunctions';
+import { createCartProductElement, createProductElement } from './helpers/shopFunctions';
 import './style.css';
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
 const productsSection = document.getElementsByClassName('products')[0];
 const loading = document.getElementsByClassName('loading')[0];
+const cartSection = document.getElementsByClassName('cart__products')[0];
+
+// CART
+
+function cartData(id) {
+  fetchProduct(id).then((results) => {
+    const cartProductElement = createCartProductElement(results);
+    cartSection
+      .appendChild(cartProductElement);
+  });
+  return saveCartID(id);
+}
+
+// PRODUCTS
 
 function callData(query) {
   const productList = fetchProductsList(query);
+
   loading.innerHTML = 'carregando...';
+
   productList.then((results) => {
     results.map((product) => {
+      const productElement = createProductElement(product);
       productsSection
-        .appendChild(createProductElement(product));
-      loading.remove();
-      return '';
+        .appendChild(productElement);
+      return productElement.lastChild.addEventListener('click', () => {
+        cartData(productElement.firstChild.innerHTML);
+      });
     });
+    loading.remove();
   });
+
   return productList;
 }
 
